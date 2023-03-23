cf0748e12f71c1a8e0dc9ef362c9dad68d204975
Samueelx <gsammygitau@gmail.com>
2023-03-23 11:25:48 +0300

    done creating login endpoint.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+    before_action :session_expired?, only: [:check_login_status]
+
     def register
         user = User.create(user_params)
         if user.valid?
@@ -8,7 +11,28 @@ class UsersController < ApplicationController
         end
     end
 
+    def login
+        sql = "username = :username OR email = :email"
+        user = User.where(sql, {username: user_params[:username], email: user_params[:email] }).first
+        if user&.authenticate(user_params[:password])
+            save_user(id: user.id)
+            app_response(message: 'login was successful', status: :ok, data: user)
+        else
+            app_response(message: 'invalid username/email or password', status: :unauthorized)
+        end
+    end
+
+    def logout
+        remove_user
+        app_response(message: 'Logout successful', status: :ok)
+    end
+
+    def check_login_status
+        app_response(message: 'Success', status: :ok)
+    end
+
     private
+
     def user_params
         params.permit(:username, :email, :password)
     end
