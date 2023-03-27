5499cd9bacd9e5c3f6c79531a75ba9b989765ac4
Samueelx <gsammygitau@gmail.com>
2023-03-27 12:05:42 +0300

    add bearer auth mechanism using jwt.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,51 @@ class ApplicationController < ActionController::API
         }, status: status
     end
 
+    #hash data into a web token
+    def encode(uid, email)
+        payload = {
+            data: {
+                uid: uid,
+                email: email
+            }, 
+            exp: Time.now.to_i + (6 * 3600)
+        }
+
+        begin
+            JWT.encode(payload, ENV['task_train_key'], 'HS256')
+        rescue JWT::EncodeError => e
+            app_response(message: 'failed', status: 400, data: {info: 'Something went wrong, please try again...'})   
+        end
+    end
+
+    def decode(token)
+        begin
+            JWT.decode(token, ENV['task_train_key'], true, {algorithm: 'HS256'}) 
+        rescue JWT::DecodeError => e
+            app_response(message: 'failed', status: 401, data: {info: 'Your session has expired, please login to continue'})
+        end
+    end
+
+    #verify authentication headers
+    def verify_auth
+        auth_headers = request.headers['Authorization']
+        unless auth_headers
+            return app_response(message: 'failed', status: 401, data: {info: 'Your request is not authorized'})
+        end
+        token = auth_headers.split(' ')[1]
+        save_user_id(token)
+    end
+
+    # request = {
+    #     headers: {
+    #         Authorization: 'Bearer token_here'
+    #     },
+    #     body: {
+
+    #     },
+    #     method: 'POST'
+    # }
+
     #store user id in session
     def save_user(id)
         session[:uid] = id
@@ -32,6 +77,15 @@ class ApplicationController < ActionController::API
 
     #get logged in user id
     def user
+        User.find(@uid)
+    end
+
+    def save_user_id(token)
+        @uid = decode(token)[0]["data"]["uid"].to_i  
+    end
+
+    #get logged in user
+    def user_session
         User.find(session[:uid].to_i)
     end
 
